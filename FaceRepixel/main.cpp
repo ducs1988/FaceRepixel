2468be7c435924071b4ba45b56e832fef895cb37
Chenshi Du <CS@public-172-21-223-65.near.uiuc.edu>
2012-11-02 19:19:43 -0500

    Initial Commit

---
+++ FaceRepixel/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FaceRepixel
+//
+//  Created by Chenshi Du on 11/2/12.
+//  Copyright (c) 2012 IIT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
