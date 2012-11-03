cb1de8b8d6a84a417a7435b50d9e2f299fb4a31b
ducs1988 <ducs1988@gmail.com>
2012-11-02 21:43:50 -0500

    Changred util

--- FaceRepixel/util.cpp
+++ FaceRepixel/util.cpp
@@ -8,3 +8,28 @@
 
 #include "fr.h"
 
+double computeAvg(Mat &cimg, int xstart, int ystart, int xend, int yend) {
+    double avg = 0.0;
+    double sumB = 0.0;
+    double sumG = 0.0;
+    double sumR = 0.0;
+    
+    for (int i=xstart; i<=xend; i++) {
+        for (int j=ystart; j<yend; j++) {
+            sumB += cimg.at<Vec3b>(i,j)[0];
+            sumG += cimg.at<Vec3b>(i,j)[1];
+            sumR += cimg.at<Vec3b>(i,j)[2];
+        }
+    }
+    
+    
+    
+    return avg;
+}
+
+double computeVar(Mat &cimg, int xstart, int ystart, int xend, int yend, double avg) {
+    double var=0;
+    
+    
+    return var;
+}
